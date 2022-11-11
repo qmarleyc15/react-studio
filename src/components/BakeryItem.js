f98a0d2bbba93efc5096d1c08aef2107e57316c3
qmarleyc15 <77942213+qmarleyc15@users.noreply.github.com>
2022-11-10 21:16:22 -0500

    Update BakeryItem.js

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,14 @@
 // TODO: create a component that displays a single bakery item
+// TODO: create a component that displays a single bakery item
+import "./BakeryItem.css"
+function BakeryItem(props){
+    return (<div className="BakeryItem"> 
+            <img src = {props.item.image}/>
+         <h1>{props.item.name}</h1>
+         <p>Price:{props.item.price}</p>
+         <p>{props.item.description}</p>
+         <button onClick={() => {props.addtoCart(props.item)}}> Add to Cart</button>
+    </div>)
+
+    }
+    export default BakeryItem;
